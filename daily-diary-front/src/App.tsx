89baf164f5f1001ed5f9d7b3c614e6ad54f76648
ToeToeAung <toetoeag@gmail.com>
2024-09-25 16:57:50 -0500

    Added login features

--- daily-diary-front/src/App.tsx
+++ daily-diary-front/src/App.tsx
@@ -1,9 +1,71 @@
 import AppRoutes from "./routes/AppRoutes";
+import React, { useState, useEffect } from 'react';
+import Login from './Login'
+import {
+    Container,
+    Card,  
+    CardContent, 
+    Button,   
+  } from '@mui/material';
+  import { ExitToApp as ExitToAppIcon, } from '@mui/icons-material';
+
 
 function App() {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [currentUser, setCurrentUser] = useState<{ username: string; name: string } | null>(null);
+
+    useEffect(() => {
+        const authStatus = localStorage.getItem('isAuthenticated');
+        const loggedInUser = localStorage.getItem('loggedInUser');        
+        
+        if (authStatus === 'true' && loggedInUser) {
+          setIsAuthenticated(true);
+          setCurrentUser(JSON.parse(loggedInUser)); 
+        }
+      }, []);
+  
+
+      const handleLogout = () => {
+        localStorage.removeItem('isAuthenticated');
+        localStorage.removeItem('loggedInUser');
+        localStorage.removeItem('uname')
+        localStorage.removeItem('uid')
+
+        setIsAuthenticated(false);
+        setCurrentUser(null);
+    
+      };
+      
+      const handleLogin = (user: { username: string; name: string }) => {
+        setIsAuthenticated(true);
+        setCurrentUser(user);
+      };
+
+
     return (
         <div className="App">
-            <AppRoutes />
+            {isAuthenticated ? (
+           <div>      
+                <AppRoutes />        
+                <Container maxWidth="xl">
+                    <Card  sx={{ m: 2, p: 2 }}>             
+                        <CardContent>
+                        <Button         
+                        onClick={handleLogout}       
+                        variant="contained"
+                        startIcon={<ExitToAppIcon />}                  
+                        sx={{ ml: 1, mt: 1 }}
+                        >
+                        Logout
+                        </Button>
+                    
+                        </CardContent>
+                    </Card>
+                </Container>        
+          </div>
+          ) : (
+             <Login onLogin={handleLogin} />
+         )}
         </div>
     );
 }
---
+++ daily-diary-front/src/Login.tsx
@@ -0,0 +1,127 @@
+import React, { useState,useEffect } from 'react';
+import userImg from '../src/assets/images/user.png'
+import {
+  Container,
+  Card,
+  CardHeader,
+  CardContent,
+  TextField,
+  Button,
+  Box,
+  Avatar
+} from '@mui/material';
+import { SendOutlined as SendOutlinedIcon } from '@mui/icons-material';
+
+
+interface LoginProps {
+    onLogin: (user: { username: string; name: string }) => void;
+  }
+
+const Login = ({ onLogin } :LoginProps) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const usernameInput = document.querySelector('input[name="username"]');
+    if (usernameInput) {
+      (usernameInput as HTMLInputElement).focus(); // Set focus on the input
+    }
+  }, []);
+
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const dummyUsers = [
+        { uid:101 ,username: 'admin', password: '123', name: 'Admin User' },
+        { uid:102 ,username: 'john', password: '123', name: 'John Doe' },
+        { uid:103 ,username: 'jane', password: '123', name: 'Jane Doe' },
+      ];
+
+      const user = dummyUsers.find(
+        (u) => u.username === username && u.password === password
+      );
+
+    
+      if (user) {     
+        localStorage.setItem('isAuthenticated', 'true');
+        localStorage.setItem('loggedInUser', JSON.stringify(user));
+        localStorage.setItem('uname',user.username)
+        localStorage.setItem('uid',user.uid.toString())
+
+        onLogin(user);
+      } else {
+        setError('Invalid username or password');
+      }
+  };
+
+  return (
+      <div>
+      <Container maxWidth="sm">
+        <Card variant="outlined" sx={{ m: 2, p: 2 }}>
+          <CardHeader 
+          avatar={
+            <Avatar
+              alt="Login Image"
+              src={userImg}
+            />
+          }
+          title="Login" />
+          <CardContent>
+            <Box
+              component="form"
+              sx={{
+                '& .MuiTextField-root': {
+                  mt: 1,
+                  mb: 1,
+                  width: '100%',
+                },
+                Button: { mt: 1 },
+              }}
+              noValidate
+              autoComplete="off"
+              onSubmit={handleSubmit}
+            >
+              <div>
+                <TextField
+                  required
+                  fullWidth
+                  label="Username"
+                  name="username"
+                  value={username}
+                  placeholder="Enter username"               
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div>
+                <TextField
+                  required
+                  fullWidth
+                  label="Password"
+                  type="password"
+                  name="password"
+                  value={password}         
+                  onChange={(e) => setPassword(e.target.value)}        
+                />
+              </div>
+              <div>         
+  
+                <Button                
+                  variant="contained"
+                  startIcon={<SendOutlinedIcon />}
+                  type="submit"
+                  sx={{ ml: 1, mt: 1 }}
+                >
+                  Login
+                </Button>
+              </div>
+            </Box>
+          </CardContent>
+        </Card>
+      </Container>
+      </div>
+    );
+};
+
+export default Login;
